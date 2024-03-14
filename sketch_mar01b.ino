dbf21d03f65f64fefb61985e40d04d5f9ae38ea8
Chris <pixelnightly@proton.me>
2024-03-14 13:05:55 +0200

    Update sketch_mar01b.ino

--- sketch_mar01b.ino
+++ sketch_mar01b.ino
@@ -1,18 +1,31 @@
-#define MOTOR_R_B 3
+#define MOTOR_R_B 4
 #define MOTOR_L_B 2
-#define MOTOR_R_F 4
-#define MOTOR_L_F 5
+#define MOTOR_R_F 5
+#define MOTOR_L_F 3
+#define LEFT_T 8
+#define LEFT_E 9
 
 void setup() {
   pinMode(MOTOR_L_F, OUTPUT);
   pinMode(MOTOR_L_B, OUTPUT);
   pinMode(MOTOR_R_F, OUTPUT);
   pinMode(MOTOR_R_B, OUTPUT);
+  pinMode(LEFT_E, INPUT);
+  pinMode(LEFT_T, OUTPUT);
+  Serial.begin(9600);
 }
 
 void loop() {
-  //goLeft();
-  delay(1000);
+  float left = (checkLeftSensor()*.0343)/2;
+  
+  Serial.write("L: ");
+  Serial.println(left);
+  if (left < 5.0f) {
+    goLeft();
+  } else {
+    goToHell();
+  }
+  delay(100);
 }
 
 float checkLeftSensor() {
@@ -21,16 +34,7 @@ float checkLeftSensor() {
   digitalWrite(LEFT_T, HIGH);
   delayMicroseconds(10);
   digitalWrite(LEFT_T, LOW);
-  return float(pulseIn(LEFT_E, HIGH)) * SENSOR_CALC;
-}
-
-float checkFrontSensor() {
-  digitalWrite(FRONT_T, LOW);
-  delayMicroseconds(2);
-  digitalWrite(FRONT_T, HIGH);
-  delayMicroseconds(10);
-  digitalWrite(FRONT_T, LOW);
-  return float(pulseIn(FRONT_E, HIGH)) * SENSOR_CALC;
+  return float(pulseIn(LEFT_E, HIGH));
 }
 
 void goForwards() {
@@ -59,4 +63,11 @@ void goRight() {
   digitalWrite(MOTOR_L_B, LOW);
   digitalWrite(MOTOR_R_F, LOW);
   digitalWrite(MOTOR_R_B, HIGH);
+}
+
+void goToHell() {
+  digitalWrite(MOTOR_L_F, LOW);
+  digitalWrite(MOTOR_L_B, LOW);
+  digitalWrite(MOTOR_R_F, LOW);
+  digitalWrite(MOTOR_R_B, LOW);
 }
